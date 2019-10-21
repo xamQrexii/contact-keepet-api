1c75c52f7d6a8f4239851c4d969932a6856e572e
xamqrexii <usama1630@gmail.com>
2019-10-22 01:59:22 +0500

    Added contact routes

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json({ extended: false }));
 // Define Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/user'));
+app.use('/api/contacts', require('./routes/contact'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
