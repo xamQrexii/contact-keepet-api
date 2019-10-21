d8915957569d9ebf0fba546050e37169224cc94f
xamqrexii <usama1630@gmail.com>
2019-10-22 01:26:52 +0500

    Database connection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const connectDB = require('./config/db');
+
+// Connect Database
+connectDB();
+
 // Init Middleware
 app.use(express.json({ extended: false }));
 
