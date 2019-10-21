050defb7010929e5f1721dc4a1bb10c04c6af019
xamqrexii <usama1630@gmail.com>
2019-10-22 01:41:24 +0500

    Added auth routes

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-
+// Define Routes
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
