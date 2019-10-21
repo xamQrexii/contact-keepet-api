9b4fbb2c4699a8f9c82d7b113dee0d8c6a36702e
xamqrexii <usama1630@gmail.com>
2019-10-22 01:44:32 +0500

    Added user routes

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/user'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
